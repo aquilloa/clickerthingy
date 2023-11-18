dde7fc6496c3435691be0e2bb46e4a064d089712
KungFo0Monkee <143532511+KungFo0Monkee@users.noreply.github.com>
2023-11-17 20:17:51 -0600

    Update autoclicker.js

--- autoclicker.js
+++ autoclicker.js
@@ -1,1 +1,20 @@
+// autoClicker.js
+
+// Add this line at the beginning of the file to declare autoClickInterval
+let autoClickInterval;
+
+function startAutoClicker(autoClickRate) {
+    // Clear existing auto-click interval if any
+    clearInterval(autoClickInterval);
+
+    // Start a new auto-click interval using the provided autoClickRate
+    autoClickInterval = setInterval(function () {
+        clickHandler();
+    }, 1000 / autoClickRate);
+}
+
+function stopAutoClicker() {
+    // Clear the auto-click interval
+    clearInterval(autoClickInterval);
+}
 
